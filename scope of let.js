80e1707102ffa90830ea8ef8b09df86f60bc8911
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2021-09-10 00:23:32 +0530

    Create scope of let.js

---
+++ scope of let.js
@@ -0,0 +1,13 @@
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+/*output
+Block scope i is:  block scope
+Function scope i is:  function scope*/
