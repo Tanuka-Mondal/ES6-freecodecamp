42b94873a9daa610fceb54c2b407c6be523aca71
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2021-09-11 20:55:07 +0530

    Create Rest Parameter with Function Parameters.js

---
+++ Rest Parameter with Function Parameters.js
@@ -0,0 +1,14 @@
+const sum = (...args) => {
+  //const args = [x, y, z];
+  return args.reduce((a, b) => a + b, 0);
+}
+console.log(sum(0,1,2))
+console.log(sum(1,2,3,4))
+console.log(sum(5))
+console.log(sum())
+
+/* output
+3
+10
+5
+0 */
